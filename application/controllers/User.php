3c5d69b46e62042ffd0da6663109ba102c6b7722
hemanth131 <mvenkatahemanth@gmail.com>
2015-07-06 11:04:25 +0530

    xssclean

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -95,7 +95,7 @@ class User extends CI_Controller{
           
           $this->load->library('form_validation');
           // field name, error message, validation rules
-          $this->form_validation->set_rules('user_name', 'User Name', 'trim|required|min_length[4]|xss_clean');
+          $this->form_validation->set_rules('user_name', 'User Name', 'trim|required|min_length[4]');
           $this->form_validation->set_rules('email_address', 'Your Email', 'trim|required|valid_email');
           $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[32]');
           $this->form_validation->set_rules('con_password', 'Password Confirmation', 'trim|required|matches[password]');
