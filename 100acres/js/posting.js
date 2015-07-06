b99689c04ba1702058c460337b173a77c7bd3a86
ashdhee <dheerajgoyal38@gmail.com>
2015-07-06 16:29:42 +0530

    compressed javascripts and css

--- 100acres/js/posting.js
+++ 100acres/js/posting.js
@@ -1,8 +1,1 @@
-function removeProp(pid,prop_type)
-{
-		
-		$.post("http://www.100acres.com/index.php/welcome/deletePosting",{pid:pid,ptype:prop_type},function(data, status){
-			
-		});	
-		window.location.assign("http://www.100acres.com/index.php/welcome/gotoPostings");
-}
+function removeProp(a,b){$.post("http://www.100acres.com/index.php/welcome/deletePosting",{pid:a,ptype:b},function(d,c){});window.location.assign("http://www.100acres.com/index.php/welcome/gotoPostings")};
